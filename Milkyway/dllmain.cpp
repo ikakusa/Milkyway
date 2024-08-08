ab92c718493943340ce1f13cefeb7c4c00ad478c
誠 遺憾 <ikasan2860@gmail.com>
2024-08-08 20:04:04 +0900

    プロジェクト ファイルを追加します。

---
+++ Milkyway/dllmain.cpp
@@ -0,0 +1,24 @@
+﻿#include "dllmain.h"
+
+DWORD WINAPI init(LPVOID lpParam) {
+    return 0;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)init, hModule, NULL, NULL);
+        DisableThreadLibraryCalls(hModule);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
